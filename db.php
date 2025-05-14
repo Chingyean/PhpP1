c5439224dd55e966741537dda620a3e12e51282b
Chingyean <100819829+Chingyean@users.noreply.github.com>
2025-05-14 23:37:43 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '1234';
-$database = 'productdb';
+$host = "sql106.byetcluster.com";
+$username = "if0_38834353"; 
+$password = "Tesla3691801";
+$dbname = "if0_38834353_Php_Project1";
 
 $conn = new mysqli($host, $user, $password, $database);
 
